kimarakov/webdriver

commit b94536accf7d07926cbe28f6379ab9cd6bef1a7e

Setting of just window position or size instead of both using 'setWindowRect' (#3216)
* Support specifying 'null' as expected parameter type for validation.

* Allow setting of size or position value(s) of browser window instead of always requiring all size/position values.

packages/webdriver/src/utils.js
@@ -97,7 +97,8 @@ export function isValidParameter (arg, expectedType) {
     }
 
     for (const argEntity of arg) {
-        if (!(typeof argEntity).match(expectedType)) {
+        const argEntityType = argEntity === null ? 'null' : typeof argEntity
+        if (!argEntityType.match(expectedType)) {
             return false
         }
     }

packages/webdriver/tests/utils.test.js
@@ -27,10 +27,16 @@ describe('utils', () => {
         expect(isValidParameter(1, 'number')).toBe(true)
         expect(isValidParameter(1, 'number[]')).toBe(false)
         expect(isValidParameter([1], 'number[]')).toBe(true)
+        expect(isValidParameter(null, 'null')).toBe(true)
+        expect(isValidParameter('', 'null')).toBe(false)
+        expect(isValidParameter(undefined, 'null')).toBe(false)
+        expect(isValidParameter({}, 'object')).toBe(true)
+        expect(isValidParameter(null, 'object')).toBe(false)
         expect(isValidParameter(1, '(number|string|object)')).toBe(true)
         expect(isValidParameter('1', '(number|string|object)')).toBe(true)
         expect(isValidParameter({}, '(number|string|object)')).toBe(true)
         expect(isValidParameter(false, '(number|string|object)')).toBe(false)
+        expect(isValidParameter(null, '(number|string|object)')).toBe(false)
         expect(isValidParameter(1, '(number|string|object)[]')).toBe(false)
         expect(isValidParameter('1', '(number|string|object)[]')).toBe(false)
         expect(isValidParameter({}, '(number|string|object)[]')).toBe(false)
@@ -38,6 +44,7 @@ describe('utils', () => {
         expect(isValidParameter([1], '(number|string|object)[]')).toBe(true)
         expect(isValidParameter(['1'], '(number|string|object)[]')).toBe(true)
         expect(isValidParameter([{}], '(number|string|object)[]')).toBe(true)
+        expect(isValidParameter([null], '(number|string|object)[]')).toBe(false)
         expect(isValidParameter([false], '(number|string|object)[]')).toBe(false)
         expect(isValidParameter(['1', false], '(number|string|object)[]')).toBe(false)
     })

commit 81c7cac294f5d4ac213f7520b75cfe8a5254404f

Unable pass null value to switchToFrame command for WebDriver (#3239)
## Proposed changes

* Resolves issue with `switchToFrame` command not accepting `null` as valid value.

## Types of changes
- [x] Bugfix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)

## Checklist
- [x] I have read the [CONTRIBUTING](https://github.com/webdriverio/webdriverio/blob/master/CONTRIBUTING.md) doc
- [x] I have added tests that prove my fix is effective or that my feature works
- [ ] I have added necessary documentation (if appropriate)

## Further comments

Issue was introduced with #3216, due to the fact `null` is actually properly validated (matched to `object` before).

Double checked the W3C standard specification and `switchToFrame` is the only other command which accepts `null` as value, next to `setWindowRect`.

### Reviewers: @webdriverio/technical-committee

packages/webdriver/src/command.js
@@ -1,6 +1,6 @@
 import logger from '@wdio/logger'
 import WebDriverRequest from './request'
-import { isValidParameter, commandCallStructure } from './utils'
+import { isValidParameter, getArgumentType, commandCallStructure } from './utils'
 
 const log = logger('webdriver')
 
@@ -55,7 +55,7 @@ export default function (method, endpointUri, commandInfo) {
                 throw new Error(
                     `Malformed type for "${commandParam.name}" parameter of command ${command}\n` +
                     `Expected: ${commandParam.type}\n` +
-                    `Actual: ${typeof arg}` +
+                    `Actual: ${getArgumentType(arg)}` +
                     moreInfo
                 )
             }

packages/webdriver/src/utils.js
@@ -97,13 +97,20 @@ export function isValidParameter (arg, expectedType) {
     }
 
     for (const argEntity of arg) {
-        const argEntityType = argEntity === null ? 'null' : typeof argEntity
+        const argEntityType = getArgumentType(argEntity)
         if (!argEntityType.match(expectedType)) {
             return false
         }
     }
 
     return true
+}
+
+/**
+ * get type of command argument
+ */
+export function getArgumentType (arg) {
+    return arg === null ? 'null' : typeof arg
 }
 
 /**

packages/webdriver/tests/utils.test.js
@@ -1,5 +1,5 @@
 import {
-    isSuccessfulResponse, isValidParameter, getPrototype, commandCallStructure, isW3CSession,
+    isSuccessfulResponse, isValidParameter, getArgumentType, getPrototype, commandCallStructure, isW3CSession,
     isChromiumSession
 } from '../src/utils'
 
@@ -31,11 +31,13 @@ describe('utils', () => {
         expect(isValidParameter('', 'null')).toBe(false)
         expect(isValidParameter(undefined, 'null')).toBe(false)
         expect(isValidParameter({}, 'object')).toBe(true)
+        expect(isValidParameter([], 'object')).toBe(true)
         expect(isValidParameter(null, 'object')).toBe(false)
         expect(isValidParameter(1, '(number|string|object)')).toBe(true)
         expect(isValidParameter('1', '(number|string|object)')).toBe(true)
         expect(isValidParameter({}, '(number|string|object)')).toBe(true)
         expect(isValidParameter(false, '(number|string|object)')).toBe(false)
+        expect(isValidParameter([], '(number|string|object)')).toBe(true)
         expect(isValidParameter(null, '(number|string|object)')).toBe(false)
         expect(isValidParameter(1, '(number|string|object)[]')).toBe(false)
         expect(isValidParameter('1', '(number|string|object)[]')).toBe(false)
@@ -44,9 +46,21 @@ describe('utils', () => {
         expect(isValidParameter([1], '(number|string|object)[]')).toBe(true)
         expect(isValidParameter(['1'], '(number|string|object)[]')).toBe(true)
         expect(isValidParameter([{}], '(number|string|object)[]')).toBe(true)
+        expect(isValidParameter([[]], '(number|string|object)[]')).toBe(true)
         expect(isValidParameter([null], '(number|string|object)[]')).toBe(false)
         expect(isValidParameter([false], '(number|string|object)[]')).toBe(false)
         expect(isValidParameter(['1', false], '(number|string|object)[]')).toBe(false)
+    })
+
+    it('getArgumentType', () => {
+        expect(getArgumentType(1)).toBe('number')
+        expect(getArgumentType(1.2)).toBe('number')
+        expect(getArgumentType(null)).toBe('null')
+        expect(getArgumentType('text')).toBe('string')
+        expect(getArgumentType({})).toBe('object')
+        expect(getArgumentType([])).toBe('object')
+        expect(getArgumentType(true)).toBe('boolean')
+        expect(getArgumentType(false)).toBe('boolean')
     })
 
     it('getPrototype', () => {